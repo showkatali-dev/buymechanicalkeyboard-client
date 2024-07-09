cbc7ee1f44fc13fe6f551910ce46422f8f9929c0
showkatali-dev <showkatali.dev@gmail.com>
2024-07-09 23:11:54 +0600

    project initialized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-center text-primary font-semibold text-2xl">
+        Buy Mechanical Keyboard
+      </h1>
+    </div>
+  );
+};
+
+export default App;
